5e4ef2b41681caef9318b064d2a6acbd7c3fc9e3
Michael Vasios <mvasios1@gmail.com>
2017-12-03 21:22:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function addToCart(item) {
   var price = Math.floor(Math.random() *100);
   var itemWithPrice = { [item] : price};
   cart.push(itemWithPrice);
-
+  console.log(item + " has been added to your cart.")
   return cart;
 }
 
