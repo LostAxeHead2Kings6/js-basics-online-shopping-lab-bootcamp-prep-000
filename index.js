5850d4fe8545f0336b4c1e915d9fdec6e506dadd
Michael Vasios <mvasios1@gmail.com>
2017-12-03 21:23:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,8 +19,9 @@ function addToCart(item) {
 }
 
 function viewCart() {
-  // write your code here
-}
+  if (cart.length == 0) {
+    console.log("Your shopping cart is empty.")
+  }
 
 function total() {
   // write your code here
