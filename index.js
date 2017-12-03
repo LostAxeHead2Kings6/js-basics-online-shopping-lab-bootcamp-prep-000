c07f579d0ba727ab07c104f9d24f5293ecc5f91c
Michael Vasios <mvasios1@gmail.com>
2017-12-03 21:17:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,7 +12,9 @@ function setCart(c) {
 function addToCart(item) {
  // write your code here
  var price = Math.floor(Math.random() *100);
- 
+var itemWithPrice = {item : price};
+cart.push(itemWithPrice);
+
 }
 
 function viewCart() {
